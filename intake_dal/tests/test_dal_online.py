4511dcd219f4a2ed37b59c65fa7704275cbd3871
Sotos Matzanas <smatzanas@zillowgroup.com>
2019-12-12 15:59:55 -0800

    [AIP-294] Update intake_online dal to read Json

--- intake_dal/tests/test_dal_online.py
+++ intake_dal/tests/test_dal_online.py
@@ -1,16 +1,15 @@
 import datetime
 import json
+from typing import List, Dict
 from unittest import mock
 from unittest.mock import MagicMock
 
 import pandas as pd
 import pytest
-from pandas.util.testing import assert_frame_equal
 
 from intake_dal.dal_catalog import DalCatalog
 from intake_dal.dal_online import (
     DalOnlineSource,
-    deserialize_avro_str_to_pandas,
     serialize_panda_df_to_str,
 )
 
@@ -27,32 +26,51 @@ def user_events_df():
     )
 
 
+@pytest.fixture
+def user_events_json():
+    return [{
+        "userid": 100,
+        "home_id": 3,
+        "action": "click",
+        "timestamp": {
+            "format": "DATETIME",
+            "time": "2012-05-01T00:00:00.000000000",
+        }
+    },
+        {
+            "userid": 101,
+            "home_id": 4,
+            "action": "click",
+            "timestamp": {
+                "format": "DATETIME",
+                "time": "2012-05-02T00:00:00.000000000",
+            }
+        }
+    ]
+
+
 @mock.patch("intake_dal.dal_online._http_put_avro_data_set")
 @mock.patch("intake_dal.dal_online._http_get_avro_data_set")
 def test_dal_online_write_read(
-    mock_get: MagicMock, mock_put: MagicMock, serving_cat: DalCatalog, user_events_df: pd.DataFrame
+        mock_get: MagicMock, mock_put: MagicMock, serving_cat: DalCatalog, user_events_df: pd.DataFrame,
+        user_events_json: List[Dict]
 ):
     canonical_name = "entity.user.user_events"
     avro_str = serialize_panda_df_to_str(
         user_events_df, schema=json.loads(serving_cat.metadata["data_schema"][canonical_name])
     )
-    mock_get.return_value = avro_str
+    mock_get.return_value = user_events_json
     mock_put.return_value = 200
 
     serving_cat.entity.user.user_events(storage_mode="serving").write(user_events_df)
     assert serving_cat.entity.user.user_events(key=100).read().iloc[0].userid == 100
     assert serving_cat.entity.user.user_events(key=100).read().iloc[0].home_id == 3
 
-    assert_frame_equal(user_events_df, serving_cat.entity.user.user_events(key=100).read(), check_dtype=False)
+    assert serving_cat.entity.user.user_events(key=100).read().iloc[1].userid == 101
+    assert serving_cat.entity.user.user_events(key=100).read().iloc[1].home_id == 4
 
     mock_get.assert_called()
     mock_put.assert_called()
-
-    assert_frame_equal(
-        user_events_df,
-        deserialize_avro_str_to_pandas(mock_put.call_args[0][1]["avro_rows"]),
-        check_dtype=False,
-    )
 
     assert avro_str != mock_put.call_args[0][1]["avro_rows"]  # not sure why!
 
@@ -66,14 +84,14 @@ def test_dal_online_write_read(
 
 def test_dal_write_parallelism(serving_cat: DalCatalog):
     assert (
-        serving_cat["entity.user.user_events"].discover()["metadata"][DalOnlineSource.name][
-            "write_parallelism"
-        ]
-        == 2
+            serving_cat["entity.user.user_events"].discover()["metadata"][DalOnlineSource.name][
+                "write_parallelism"
+            ]
+            == 2
     )
     assert (
-        serving_cat.entity.user.user_events.discover()["metadata"][DalOnlineSource.name]["write_parallelism"]
-        == 2
+            serving_cat.entity.user.user_events.discover()["metadata"][DalOnlineSource.name]["write_parallelism"]
+            == 2
     )
 
 
